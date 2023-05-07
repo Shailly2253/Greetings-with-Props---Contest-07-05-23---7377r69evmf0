eb8b5dc44ec0f16901e28d63ca62c2afe888475e
CodewithShailly <121444727+Shailly2253@users.noreply.github.com>
2023-05-07 13:13:20 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,15 +2,15 @@
 import React from 'react';
 
 const Welcome = ({name}) => {
-  let sayHi = 'Hey ${name}';
-  let greetMessage = 'Welcome to Newton School.';
+  let sayHi = `Hey ${name}!`;
+  let greetMessage = `Welcome to Newton School.`;
   return (
     <div>
-       <h1>{sayHi}</h1>
-       <h2>{greetMessage}</h2>
+      <h1>{sayHi}</h1>
+      <h2>{greetMessage}</h2>
     </div>
-  );
-);
-  
+   );
+};
+
 export default Welcome;
     
